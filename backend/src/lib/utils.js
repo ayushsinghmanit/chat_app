6820e53c6de2235757e5af4d7861cf6b09354ac7
Ayush Singh <ayushsinghnandi@gmail.com>
2025-07-07 15:16:36 +0530

    Update utils.js

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -1,3 +1,19 @@
+// import jwt from "jsonwebtoken";
+
+// export const generateToken = (userId, res) => {
+//   const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+//     expiresIn: "7d",
+//   });
+
+//   res.cookie("jwt", token, {
+//     maxAge: 7 * 24 * 60 * 60 * 1000, // MS
+//     httpOnly: true, // prevent XSS attacks cross-site scripting attacks
+//     sameSite: "strict", // CSRF attacks cross-site request forgery attacks
+//     secure: process.env.NODE_ENV !== "development",
+//   });
+
+//   return token;
+// };
 import jwt from "jsonwebtoken";
 
 export const generateToken = (userId, res) => {
@@ -8,9 +24,10 @@ export const generateToken = (userId, res) => {
   res.cookie("jwt", token, {
     maxAge: 7 * 24 * 60 * 60 * 1000, // MS
     httpOnly: true, // prevent XSS attacks cross-site scripting attacks
-    sameSite: "strict", // CSRF attacks cross-site request forgery attacks
-    secure: process.env.NODE_ENV !== "development",
+    sameSite: "None", // CSRF attacks cross-site request forgery attacks
+    secure: true, 
   });
 
   return token;
 };
+
