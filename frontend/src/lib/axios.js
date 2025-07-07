855cdc62768a640648196c7e188662e8a6b2fc6c
Ayush Singh <ayushsinghnandi@gmail.com>
2025-07-07 15:28:52 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,11 @@
-import axios from "axios";
+// import axios from "axios";
 
+// export const axiosInstance = axios.create({
+//   baseURL: import.meta.env.MODE === "development" ? "http://localhost:5000/api" : "/api",
+//   withCredentials: true,
+// });
+import axios from "axios";
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5000/api" : "/api",
+  baseURL: "https://chat-app-2-atif.onrender.com/api",
   withCredentials: true,
 });
